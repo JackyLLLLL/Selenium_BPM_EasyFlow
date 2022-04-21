3f5d757bfc869583f80f019d54bfe9e356ee9a4f
JackyLLLLL <lkktwn8331@yahoo.com.tw>
2022-04-21 17:28:07 +0800

    220421 update

--- Selenium_BPM_EasyFlow.py
+++ Selenium_BPM_EasyFlow.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 from bs4 import BeautifulSoup
 
-
 """
 by_id -> find_element("id","")
 
@@ -31,13 +30,16 @@ url = r"http://bpm.hec-group.com.tw:8086/NaNaWeb/GP//ForwardIndex?hdnMethod=find
 
 # 開啟瀏覽器視窗(Chrome)
 # 方法一：執行前需開啟chromedriver.exe且與執行檔在同一個工作目錄
+
 driver = webdriver.Chrome()
 driver.get(url)
-#driver.maximize_window()
+#driver.maximize_window() #視窗最大化
+
 job_number_xpath = (r"""//*[@id="txtUserId"]""")
 password_xpath = (r"""//*[@id="txtPassword"]""")
 job_number = "W220002"
 password = "Rockerokandko2!"
+process_subject = "Test Selenium"
 
 driver.find_element("xpath",job_number_xpath).send_keys(job_number)
 driver.find_element("xpath",password_xpath).send_keys(password)
@@ -47,31 +49,40 @@ driver.find_element("xpath",login_xpath).click()
 
 print("登入成功")
 
-
-####################################################
-
-# 獲得開啟的第一個視窗控制代碼
-window_1 = driver.current_window_handle
-# 獲得開啟的所有的視窗控制代碼
-windows = driver.window_handles
-# 切換到最新的視窗
-for current_window in windows:
-    #print(current_window)
-    #print(window_1,windows)
-    if current_window != window_1:
-        driver.switch_to.window(current_window)
-        print("切換視窗成功")
-####################################################
-
 driver.execute_script("javascript:prepareProcessData('PART','BPMN')")#點擊料號申請單(JavaScript)
 print("點擊料號申請單")
 driver.implicitly_wait(1)#等待網站載入(最多等6秒
-#driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS)
+driver.switch_to.frame("ifmFucntionLocation") #切換到嵌入的網頁
+driver.find_element("name","txtSubject").send_keys(process_subject) #輸入流程主旨
 
-container = driver.find_element("id","container")
-x=container.find_element("id","tblMenuContent")
-print(x)
-print("選擇外銷")
+
+driver.switch_to.frame("ifmAppLocation") #切換到嵌入的網頁
+driver.find_element("xpath","""//*[@id="rbSales_1"]""").click() #點擊外銷單選框
+driver.find_element("xpath","""//*[@id="rbAppType_1"]""").click() #點選外購單選框
+driver.find_element("xpath","""//*[@id="rbFactory"]/tbody/tr/td[4]/label""").click() #點選Cougar專用
+driver.find_element("xpath","""//*[@id="rbAppType3_0"]""").click() #點選 CASE
+driver.find_element("xpath","""//*[@id="rbItemType2"]/tbody/tr/td[1]/label""").click() #點選商品(買進賣出)
+driver.find_element("xpath","""//*[@id="rbBOM_0"]""").click() #不建立下階BOM表
+driver.find_element("xpath","""//*[@id="rbBOM2_0"]""").click() #不轉內外銷BOM表
+driver.find_element("xpath","""//*[@id="rbOPT_0"]""").click() #不同步拋轉至力韡
+
+#申請備品的話客戶料號無須填寫
+
+driver.find_element("xpath","""//*[@id="rbProjectCode3_0"]""").click() #申請依據點選其他
+
+driver.find_element("name","txtDescription").send_keys("這邊未來撈Execl資料") #產品描述
+driver.find_element("xpath","""//*[@id="cbPlusS_0"]""").click() #把尾碼+S給打勾起來
+driver.find_element("xpath","""//*[@id="btnReference_LFE"]""").click() #點選參考料號
+windows = driver.window_handles
+driver.switch_to.window(windows[-1]) #切換到當前最新的視窗
+
+
+driver.find_element("xpath","""//*[@id="_cuzDataChooser_criteria_0"]""").send_keys("3MGC1NXB.0001S") #查詢參考料號
+driver.find_element("xpath","""//*[@id="_btnCustomDataChooser_query"]""").click() #點擊搜尋
+
+#driver.find_element("xpath","""""").click()
+
+print("Progarm finish")
 
 
 
