39d0a1b55f49ebe798289ed0afbf67cb682d18c0
JackyLLLLL <lkktwn8331@yahoo.com.tw>
2022-05-25 21:28:01 +0800

    0525

--- woo_2.py
+++ woo_2.py
@@ -7,10 +7,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 url = r"https://wootalk.today/"
-##chrome_path = r"C:\Users\User\AppData\Local\Programs\Python\Python310\Scripts\chromedriver.exe" #家裡的driver路徑
-chrome_path = r"C:\Users\jacky-lin\AppData\Local\Programs\Python\Python310\Scripts\chromedriver.exe"
+chrome_path = r"C:\Users\User\AppData\Local\Programs\Python\Python310\Scripts\chromedriver.exe" #家裡的driver路徑
+##chrome_path = r"C:\Users\jacky-lin\AppData\Local\Programs\Python\Python310\Scripts\chromedriver.exe"
 user_agent = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
 
+mes = r"台南182 電愛不約"
 
 
 opt = webdriver.ChromeOptions()
@@ -29,20 +30,23 @@ script = 'Object.defineProperty(navigator,"webdriver",{get:() => false,});'
 driver.execute_script(script)
 
 driver.get(url)
+sleep(7)
 
-
-##driver.find_element("id","""menuButton""").click()
-##sleep(1)
-##driver.find_element("xpath","""/html/body/div[5]/div/ul[1]/li[1]/a""").click()
-##sleep(1)
-##driver.find_element("css selector","""input.keyword[value='成人模式']""").click()
-##sleep(1)
-##driver.find_element("css selector","""input#startButton""").click()
-
-
-start_chat = driver.find_element("css selector","""input#startButton""")
-actions = ActionChains(driver)
+driver.find_element("id","""menuButton""").click()
 sleep(1)
-actions.move_to_element(start_chat).click(start_chat)
-sleep(0.5)
-actions.perform()
+driver.find_element("xpath","""/html/body/div[5]/div/ul[1]/li[1]/a""").click()
+sleep(1)
+driver.find_element("css selector","""input.keyword[value='成人模式']""").click()
+sleep(1)
+driver.find_element("css selector","""input#startButton""").click()
+sleep(2)
+driver.find_element("id","""messageInput]""").send_keys(mes)
+
+#<input type="text" id="messageInput" placeholder="輸入訊息" autocomplete="off">
+
+##start_chat = driver.find_element("css selector","""input#startButton""")
+##actions = ActionChains(driver)
+##sleep(1)
+##actions.move_to_element(start_chat).click(start_chat)
+##sleep(0.5)
+##actions.perform()
