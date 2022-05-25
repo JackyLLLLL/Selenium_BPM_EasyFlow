7043eeccc8d8c1d44212f6b0c6ef3782b2fada39
JackyLLLLL <lkktwn8331@yahoo.com.tw>
2022-05-25 16:59:00 +0800

    220525

--- woo_2.py
+++ woo_2.py
@@ -4,19 +4,19 @@ from selenium import webdriver
 from time import sleep
 
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver import ChromeOptions
+
 
 url = r"https://wootalk.today/"
-secret_mes = r"成人模式"
-##chrome_path = r"C:\Users\User\AppData\Local\Programs\Python\Python310\Scripts\chromedriver.exe"
+##chrome_path = r"C:\Users\User\AppData\Local\Programs\Python\Python310\Scripts\chromedriver.exe" #家裡的driver路徑
 chrome_path = r"C:\Users\jacky-lin\AppData\Local\Programs\Python\Python310\Scripts\chromedriver.exe"
 user_agent = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
 
 
+
 opt = webdriver.ChromeOptions()
-opt.add_experimental_option('excludeSwitches', ['enable-automation'])
 opt.add_argument('--user-agent=%s' % user_agent)
 driver = webdriver.Chrome(chrome_path, options=opt)
+
 driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
   "source": """
     Object.defineProperty(navigator, 'webdriver', {
@@ -25,16 +25,24 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
   """
 })
 
-  
+script = 'Object.defineProperty(navigator,"webdriver",{get:() => false,});'
+driver.execute_script(script)
 
 driver.get(url)
 
 
-driver.find_element("id","""menuButton""").click()
-sleep(1)
-driver.find_element("xpath","""/html/body/div[5]/div/ul[1]/li[1]/a""").click()
-sleep(1)
-driver.find_element("css selector","""input.keyword[value='成人模式']""").click()
-sleep(2)
-driver.find_element("css selector","""input#startButton""").click()
+##driver.find_element("id","""menuButton""").click()
+##sleep(1)
+##driver.find_element("xpath","""/html/body/div[5]/div/ul[1]/li[1]/a""").click()
+##sleep(1)
+##driver.find_element("css selector","""input.keyword[value='成人模式']""").click()
+##sleep(1)
+##driver.find_element("css selector","""input#startButton""").click()
 
+
+start_chat = driver.find_element("css selector","""input#startButton""")
+actions = ActionChains(driver)
+sleep(1)
+actions.move_to_element(start_chat).click(start_chat)
+sleep(0.5)
+actions.perform()
